825d19bb43298f55156f17a220dc8675b7201a5e
Mukhamed <mukhamed.dzhanbekov@mail.ru>
2022-05-23 16:54:55 +0600

    finished news, help, aboutus, public offer

--- info/admin.py
+++ info/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import AboutUs, News, Help, Offer
 
-# Register your models here.
+admin.site.register(AboutUs)
+admin.site.register(News)
+admin.site.register(Help)
+admin.site.register(Offer)
--- info/urls.py
+++ info/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path
+from .views import AboutUsView, NewsListView, NewsDetailView, OfferListView, HelpListView
 
-urlpatterns = []
+urlpatterns = [
+    path('about/', AboutUsView.as_view()),
+    path('news/', NewsListView.as_view()),
+    path('news/<int:pk>/', NewsDetailView.as_view()),
+    path('offer/', OfferListView.as_view()),
+    path('help/', HelpListView.as_view(),),
+    path('help/<int:pk>/', HelpListView.as_view(),),
+
+]
