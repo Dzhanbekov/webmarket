316f1725f2de903de7de45148cd60af8fe73742b
Mukhamed <mukhamed.dzhanbekov@mail.ru>
2022-05-23 15:43:18 +0600

    first commit

---
+++ info/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+
+class AboutUs(models.Model):
+    description = models.TextField(verbose_name='О нас')
+    image_1 = models.ImageField(upload_to='about')
+    image_2 = models.ImageField(upload_to='about')
+    image_3 = models.ImageField(upload_to='about')
+
+    def __str__(self):
+        return self.description[:20]
+
+    class Meta:
+        verbose_name = 'О нас'
+        verbose_name_plural = 'О нас'
+
+
+class News(models.Model):
+
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='news')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новости'
+        verbose_name_plural = 'Новости'
+
+
+class Help(models.Model):
+    question = models.CharField(max_length=200, verbose_name='Вопрос')
+    answer = models.TextField(verbose_name='Ответ')
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        verbose_name = 'Помощь'
+        verbose_name_plural = 'Помощь'
+
+
+class Offer(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Публичная Оферта'
+        verbose_name_plural = 'Публичная Оферта'
---
+++ items/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+
+class Collection(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование коллекции')
+    image = models.ImageField(upload_to='collection', verbose_name='Картинка')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Коллекция'
+        verbose_name_plural = 'Коллекции'
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Описание товара')
+    item_id = models.CharField(max_length=200, unique=True, verbose_name='Артикул', blank=True, null=True)
+    old_price = models.PositiveIntegerField('Цена', default=0)
+    new_price = models.PositiveIntegerField('Цена', default=0)
+    description = models.TextField(verbose_name='О Товаре')
+    size_range = models.CharField(max_length=20, verbose_name='размерный ряд')
+    amount_in = models.PositiveIntegerField('количество в линейке', default=0)
+    compound = models.CharField(max_length=200, verbose_name='Состав Ткани')
+    material = models.CharField(max_length=200, verbose_name='Материал')
+    is_in_cart = models.BooleanField(default=False)
+    collection = models.ForeignKey(Collection, models.CASCADE, verbose_name='Коллекция')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+
+class ItemImage(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='itemimage')
+    image = models.ImageField(upload_to='item')
